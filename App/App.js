33818eaf3ed57e4e30b3d1fadceab892dbc6278b
treyshedrick <treyshedrick@gmail.com>
2021-05-13 20:27:15 -0500

    add Pokedex header

---
+++ App/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Header from './Header/index';
+
+const App = () => {
+  return <Header />;
+};
+
+export default App;
---
+++ App/Header/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+module.exports = StyleSheet.create({
+  headerContainer: {
+    height: 80,
+    backgroundColor: 'red',
+  },
+  sectionTitle: {
+    textAlign: 'center',
+    paddingTop: 40,
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'white',
+  },
+});
