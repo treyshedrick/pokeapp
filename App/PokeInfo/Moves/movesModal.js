c3e606cd60323f4b603cd6d5c931103cc0d37ebc
treyshedrick <treyshedrick@gmail.com>
2021-05-20 19:23:12 -0500

    MovesModal PropTypes and call api once mounted

--- App/PokeInfo/Moves/movesModal.js
+++ App/PokeInfo/Moves/movesModal.js
@@ -1,12 +1,27 @@
 import PropTypes from 'prop-types';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Chip} from 'react-native-elements';
+import axios from 'axios';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {View, Text, ScrollView, Modal} from 'react-native';
 import styles from './styles';
 
 const MovesModal = ({move, modalAction}) => {
-  const [hideModal, shouldHideModal] = useState(false);
+  const [moveInfo, setMoveInfo] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/move/${move}`)
+      .then(response => {
+        setMoveInfo(response.data);
+        console.log('Called Pokemon API for move info');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [move]);
+
+  console.log(moveInfo);
 
   return (
     <View style={styles.centeredView}>
@@ -36,6 +51,18 @@ const MovesModal = ({move, modalAction}) => {
       </Modal>
     </View>
   );
+};
+
+MovesModal.propTypes = {
+  move: PropTypes.string,
+  modalAction: PropTypes.func,
+};
+
+MovesModal.defaultProps = {
+  moves: '',
+  modalAction: () => {
+    console.log('Default Function');
+  },
 };
 
 export default MovesModal;
