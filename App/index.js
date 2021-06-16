bef33a08fabfa34f682a4a466d2061fbe8dd23e2
treyshedrick <treyshedrick@gmail.com>
2021-06-15 19:10:58 -0500

    ensure npm run test is working correctly

--- App/index.js
+++ App/index.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import Header from './Header/index';
 import Search from './Search';
