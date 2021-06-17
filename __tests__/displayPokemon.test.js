40ef3081860205f0147f6ed3c91024740e8640ba
treyshedrick <treyshedrick@gmail.com>
2021-06-16 19:20:13 -0500

    add second displayPokemon test with mock data

--- __tests__/displayPokemon.test.js
+++ __tests__/displayPokemon.test.js
@@ -1,14 +1,27 @@
 import 'react-native';
 import React from 'react';
 import Search from '../app/Search';
+import PokeInfo from '../app/PokeInfo';
+import Basic from '../app/PokeInfo/Basic';
+import Moves from '../app/PokeInfo/Moves';
+import Stats from '../app//PokeInfo/Stats';
+import * as DITTO from './mock/ditto.json';
 import renderer from 'react-test-renderer';
 import {ActivityIndicator} from 'react-native';
+import axios from 'axios';
 
 jest.mock('axios');
+jest.useFakeTimers();
 
 it('should show loading indicator', async () => {
   const SearchComp = renderer.create(<Search />).root;
+  axios.get.mockResolvedValue();
   await expect(SearchComp.findByType(ActivityIndicator)).toBeDefined();
+});
 
-  //console.log(SearchComp);
+it('Ensure pokeinfo is rendered', async () => {
+  const dittoInfo = renderer.create(<PokeInfo info={DITTO} />).root;
+  expect(dittoInfo.findByType(Basic)).toBeDefined();
+  expect(dittoInfo.findByType(Moves)).toBeDefined();
+  expect(dittoInfo.findByType(Stats)).toBeDefined();
 });
