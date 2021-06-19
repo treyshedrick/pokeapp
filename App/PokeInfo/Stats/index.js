dc302331c0ee43afd5a4acb7111ccd1c9281868b
treyshedrick <treyshedrick@gmail.com>
2021-06-18 20:18:56 -0500

    styling for stats

--- App/PokeInfo/Stats/index.js
+++ App/PokeInfo/Stats/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import styles from './styles';
 
 const Stats = ({stats, experience}) => {
   return (
     <View>
-      <View>
-        <Text>Base Experience: {experience}</Text>
+      <View style={styles.xpRow}>
+        <Text style={styles.statFont}>Base Experience: {experience}</Text>
       </View>
-      <View>
-        <Text>Hp: {stats[0].base_stat}</Text>
-        <Text>Attack: {stats[1].base_stat}:</Text>
-        <Text>Defense: {stats[2].base_stat}</Text>
-      </View>
-      <View>
-        <Text>Sp Attack: {stats[3].base_stat}</Text>
-        <Text>Sp Defense: {stats[4].base_stat}</Text>
-        <Text>Speed: {stats[5].base_stat}</Text>
+      <View style={styles.statRow}>
+        <View flexDirection="column">
+          <Text style={styles.statFont}>Hp: {stats[0].base_stat}</Text>
+          <Text style={styles.statFont}>Speed: {stats[5].base_stat}</Text>
+        </View>
+        <View flexDirection="column">
+          <Text style={styles.statFont}>Attack: {stats[1].base_stat}:</Text>
+          <Text style={styles.statFont}>Defense: {stats[2].base_stat}</Text>
+        </View>
+        <View flexDirection="column">
+          <Text style={styles.statFont}>Sp Attack: {stats[3].base_stat}</Text>
+          <Text style={styles.statFont}>Sp Defense: {stats[4].base_stat}</Text>
+        </View>
       </View>
     </View>
   );
---
+++ App/PokeInfo/Stats/styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+module.exports = StyleSheet.create({
+  statRow: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-evenly',
+    padding: 18,
+  },
+  statFont: {
+    fontSize: 16.5,
+    padding: 2.5,
+  },
+  xpRow: {
+    paddingTop: 10,
+    alignItems: 'center',
+  },
+});
