790d90b1dbaa91396cb6328a0816f369e7726d84
treyshedrick <treyshedrick@gmail.com>
2021-06-29 21:39:14 -0500

    feat: detechs if device notch and adjusts styling

--- App/Header/index.js
+++ App/Header/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import {View, Image} from 'react-native';
 import styles from './styles';
 import PokedexImage from '../assets/pokedex.png';
+import DeviceInfo from 'react-native-device-info';
 
 const Header = () => {
+  console.log(DeviceInfo.hasNotch());
   return (
     <View style={styles.headerContainer}>
-      <Image style={styles.sectionTitle} source={PokedexImage} />
+      <Image
+        style={DeviceInfo.hasNotch() ? styles.sectionTitle : null}
+        source={PokedexImage}
+      />
     </View>
   );
 };
