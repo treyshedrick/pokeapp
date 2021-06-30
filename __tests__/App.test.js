08c49d6133e802e1a5db046a57569db76bbb56b7
treyshedrick <treyshedrick@gmail.com>
2021-06-29 21:51:03 -0500

    test: mock hasNotch function

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -9,6 +9,12 @@ import App from '../app/index';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-device-info', () => {
+  return {
+    hasNotch: jest.fn(),
+  };
+});
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
